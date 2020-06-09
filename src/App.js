d9b469de9dbf8d53a0888e85cc6796b0dde9e62d
SM UmerLatif <39081061+latifumer97@users.noreply.github.com>
2020-06-09 13:21:54 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return <div>
+    <header><h1>Assignmet#3</h1></header>
+    Hello UMER <br/>
+    Name="SM UmerLatif"<br/> 
+    Age="23<br/>
+    
+  </div>
+}
+export default App;
